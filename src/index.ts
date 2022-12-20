e17e43c7ecbfe2de9951c11ddb989d7c3e5df052
samatvalive <71274107+samatvalive@users.noreply.github.com>
2022-12-20 13:07:06 +0530

    remove main method

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { ethers } from "ethers";
 import { Endpoints } from "./app/api/Endpoints";
 import { Base } from "./app/Base";
 import { Contract } from "./app/contract/Contract";
@@ -11,34 +10,3 @@ interface DZap extends Contract {}
 applyMixins(DZap, [Endpoints, Contract]);
 
 export default DZap;
-
-
-async function startServer() {
-  console.log("start");
-
-  const provider = new ethers.providers.JsonRpcProvider("");
-
-  const client = new DZap({
-    chainId: 1377,
-    provider: provider,
-  });
-
-  const request = [
-    {
-      amount: "1000000000000000000",
-      fromTokenAddress: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063",
-      slippage: 1,
-      toTokenAddress: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
-    },
-  ]
-  client
-    .swap(request, "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063")
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-startServer();
